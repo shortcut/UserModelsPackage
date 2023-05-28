53cfe3746e89457a0017f9626ecd59b3883255cd
Ivan C Myrvold <ivan@myrvold.org>
2023-05-28 06:30:11 +0200

    added httpBody computed property to RegisterRequest

--- Sources/UserModelsPackage/RegisterRequest.swift
+++ Sources/UserModelsPackage/RegisterRequest.swift
@@ -6,3 +6,12 @@ public struct RegisterRequest: Codable {
     public let email: String
     public let password: String
 }
+
+extension RegisterRequest {
+    var httpBody: Data? {
+        let encoder = JSONEncoder()
+        let jsonData = try? encoder.encode(self)
+        
+        return jsonData
+    }
+}
