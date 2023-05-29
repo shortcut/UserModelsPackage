b3f71270f4e92fb517b37a21c70e835074aa73f9
Ivan C Myrvold <ivan@myrvold.org>
2023-05-29 15:47:58 +0200

    Added LoginInput

---
+++ Sources/UserModelsPackage/LoginInput.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ivan C Myrvold on 29/05/2023.
+//
+
+import Foundation
+
+public struct LoginInput: Codable {
+    public let email: String
+    public let password: String
+}
