89792fe0cb9398f8866ef69f3746a45013150e56
Ivan C Myrvold <ivan@myrvold.org>
2023-06-09 08:53:40 +0200

    init must be public

--- Sources/UserModelsPackage/Content/HvaKosterStrømmen.swift
+++ Sources/UserModelsPackage/Content/HvaKosterStrømmen.swift
@@ -14,6 +14,14 @@ public struct HvaKosterStrømmen: Decodable {
     public let start: Date
     public let end: Date
     
+    public init(nok: Double, eur: Double, exr: Double, start: Date, end: Date) {
+        self.nok = nok
+        self.eur = eur
+        self.exr = exr
+        self.start = start
+        self.end = end
+    }
+    
     enum CodingKeys: String, CodingKey {
         case nok = "NOK_per_kWh"
         case eur = "EUR_per_kWh"
