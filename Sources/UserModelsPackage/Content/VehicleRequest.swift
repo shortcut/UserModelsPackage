0da3d6f91c0ff649e0bfeb2f481eee0d74c4f5e9
Ivan C Myrvold <ivan@myrvold.org>
2023-09-06 14:49:25 +0200

    make initializer for VehicleName public

--- Sources/UserModelsPackage/Content/VehicleRequest.swift
+++ Sources/UserModelsPackage/Content/VehicleRequest.swift
@@ -10,6 +10,11 @@ import Foundation
 public struct VehicleName: Encodable {
     public let id: String
     public let name: String
+    
+    public init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
 
 public struct VehicleRequest: Encodable {
