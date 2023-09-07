dc4c9c5bfa9733ec00c8b10576c95de954aa43a3
Ivan C Myrvold <ivan@myrvold.org>
2023-09-07 15:24:10 +0200

    use the id of the car as is of HomeScreenEV

--- Sources/UserModelsPackage/Content/HomeScreenEV.swift
+++ Sources/UserModelsPackage/Content/HomeScreenEV.swift
@@ -8,19 +8,15 @@
 import Foundation
 
 public struct HomeScreenEV: Codable, Identifiable {
-    public let id = UUID()
+    public let id: String
     let title: String
     let text: String
     var tag = 0
     
-    public init(title: String, text: String, tag: Int = 0) {
+    public init(id: String, title: String, text: String, tag: Int = 0) {
+        self.id = id
         self.title = title
         self.text = text
         self.tag = tag
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case title
-        case text
     }
 }
