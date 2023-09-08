eb97e6974c9063b620a5a4e3e36ddad4d6d8a84b
Ivan C Myrvold <ivan@myrvold.org>
2023-09-08 08:22:38 +0200

    Make all properties public

--- Sources/UserModelsPackage/Content/HomeScreenEV.swift
+++ Sources/UserModelsPackage/Content/HomeScreenEV.swift
@@ -9,9 +9,9 @@ import Foundation
 
 public struct HomeScreenEV: Codable, Identifiable {
     public let id: String
-    let title: String
-    let text: String
-    var tag = 0
+    public let title: String
+    public let text: String
+    public var tag = 0
     
     public init(id: String, title: String, text: String, tag: Int = 0) {
         self.id = id
